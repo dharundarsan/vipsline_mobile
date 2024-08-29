d5fcefb69534078e1008d114e5bf380e686c777b
Bhaski47 <bhaskarg2003@gmail.com>
2024-08-29 11:10:19 +0530

    Added Inter Regular and Inter Bold Fonts

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {Provider} from 'react-redux';
 import {AntDesign, FontAwesome5} from '@expo/vector-icons';
-import React from 'react';
+import React, {useCallback, useEffect} from 'react';
 import CheckoutScreen from './screens/CheckoutScreen';
 import CustomDrawer from './components/common/CustomDrawer';
 import AuthScreen from './screens/AuthScreen';
@@ -14,6 +14,12 @@ import ForgetPasswordScreen from './screens/ForgetPasswordScreen';
 import store from './store/store';
 import Colors from './constants/Colors';
 import {enableScreens} from "react-native-screens";
+
+//Font And SplashScreen Imports
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+SplashScreen.preventAutoHideAsync();
+
 
 // Drawer icon imports
 import calender_icon from "./assets/icons/drawerIcons/calendar.png";
@@ -30,6 +36,7 @@ import staffs_icon from "./assets/icons/drawerIcons/staffs.png";
 import settings_icon from "./assets/icons/drawerIcons/settings.png";
 import reports_icon from "./assets/icons/drawerIcons/reports.png";
 import ClientSegmentScreen from "./screens/ClientSegmentScreen";
+import {useFonts} from "expo-font";
 
 enableScreens();
 
@@ -57,6 +64,20 @@ const CheckoutStack = () => (
 );
 
 export default function App() {
+    const [loaded, error] = useFonts({
+        'Inter-Regular': require('./assets/fonts/Inter/static/Inter_18pt-Regular.ttf'),
+        'Inter-Bold':require('./assets/fonts/Inter/static/Inter_18pt-Bold.ttf')
+    });
+
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
+    }
     return (
         <Provider store={store}>
             <SafeAreaView style={styles.safeAreaView}>
