8f7840643c0a37edf2381018edc66a7e90bb031b
Bhaski47 <bhaskarg2003@gmail.com>
2024-09-03 14:22:02 +0530

    Completed Checkout Actions UI

--- components/checkoutScreen/CheckoutSection.js
+++ components/checkoutScreen/CheckoutSection.js
@@ -10,6 +10,9 @@ import {Keyboard} from "react-native";
 import {useState, useEffect} from "react";
 import PaymentModal from "./PaymentModal";
 import DropdownModal from "../../ui/DropdownModal";
+import MiniActionTextModal from "./MiniActionTextModal";
+import DeleteClient from "../clientSegmentScreen/DeleteClientModal";
+import {loadClientInfoFromDb} from "../../store/clientInfoSlice";
 
 const CheckoutSection = (props) => {
     const [isPaymentModalVisible, setIsPaymentModalVisible] = useState(false);
@@ -21,9 +24,9 @@ const CheckoutSection = (props) => {
         checkoutDetailRow: {
             flexDirection: "row",
             justifyContent: "space-around",
-            borderBottomWidth: 1,
-            borderBottomColor: Colors.grey600,
-            borderStyle: "dashed",
+            // borderBottomWidth: 1,
+            // borderBottomColor: Colors.grey600,
+            // borderStyle: "dashed",
             paddingVertical: 5,
         },
         checkoutDetailText: {},
@@ -49,27 +52,96 @@ const CheckoutSection = (props) => {
         checkoutButtonText: {
             color: Colors.white
         },
-        primaryViewChildrenStyle:{
+        primaryViewChildrenStyle: {
             flexDirection: "row",
-            alignItems:"center"
+            alignItems: "center"
         }
     });
 
     const [isModalOpen, setIsModalOpen] = useState(false);
-
+    const [ActionModal,setActionModal] = useState(false);
+    const [title, setTitle] = useState("")
+    const [isDelete, setIsDelete] = useState(false);
+    const [data, setData] = useState([{}])
+    function openModal(title){
+        setTitle(title);
+    }
     return <View style={styles.checkoutSection}>
+        {
+            ActionModal && <MiniActionTextModal isVisible={ActionModal}
+                onCloseModal={()=>{setActionModal(false)}}
+                title={title}
+                data={data}
+            />
+        }
+        {
+            isDelete && <DeleteClient
+                isVisible={isDelete}
+                onCloseModal={() => {
+                    setIsDelete(false)
+                    // setModalVisibility(false);
+                    // console.log("Fetched")
+                    // dispatch(loadClientInfoFromDb(props.id))
+                }}
+                header={"Cancel Sale"}
+                content={"If you cancel this sale transaction will not be processed. Do you wish to exit?"}
+                onCloseClientInfoAfterDeleted={() => {
+                    // props.setVisible(false);
+                    // props.setSearchQuery("");
+                    // props.setFilterPressed("all_clients_count");
+                }}
+            />
+        }
         {
             isModalOpen ?
                 <DropdownModal
                     isVisible={isModalOpen}
-                    onCloseModal={() => { setIsModalOpen(false) }}
+                    onCloseModal={() => {
+                        setIsModalOpen(false)
+                    }}
                     dropdownItems={[
                         "Apply Discount",
                         "Add Charges",
                         "Add Sales Notes",
                         "Cancel Sales"
                     ]}
-                    onChangeValue={(value) => console.log(value)}
+                    onChangeValue={(value) => {
+                        console.log(value)
+                        if (value === "Apply Discount") {
+                            openModal("Add Discount")
+                            setData([{
+                                header:"Enter Discount",
+                                boxType:"textBox",
+                                typeToggle:1,
+                            }])
+                            setActionModal(true)
+                        } else if (value === "Add Charges") {
+                            openModal("Add extra charges")
+                            setData([
+                            {
+                                header:"Item name",
+                                boxType:"textBox",
+                                typeToggle:0,
+                            },
+                            {
+                                header:"Price",
+                                boxType:"priceBox",
+                                typeToggle:0
+                            }
+                        ])
+                            setActionModal(true)
+                        } else if (value === "Add Sales Notes") {
+                            openModal("Add a note")
+                            setData([{
+                                header:"Sales notes",
+                                boxType:"multiLineBox",
+                                typeToggle:0,
+                            }])
+                            setActionModal(true)
+                        } else if (value === "Cancel Sales") {
+                            setIsDelete(true);
+                        }
+                    }}
                     iconImage={[
                         require("../../assets/icons/checkout/actionmenu/applydiscount.png"),
                         require("../../assets/icons/checkout/actionmenu/addcharges.png"),
@@ -92,6 +164,7 @@ const CheckoutSection = (props) => {
                         <Text
                             style={[textTheme.titleMedium, styles.checkoutDetailText]}>₹ {props.data[0].total_discount_in_price}</Text>
                     </View>
+                    <Divider/>
                     <View style={styles.checkoutDetailRow}>
                         <Text style={[textTheme.titleMedium, styles.checkoutDetailText]}>Sub Total</Text>
                         <Text
@@ -107,8 +180,8 @@ const CheckoutSection = (props) => {
                     {/*    <Text style={[textTheme.titleMedium, styles.checkoutDetailText]}>₹ 5000</Text>*/}
                     {/*</View>*/}
                     <View style={styles.buttonContainer}>
-                        <PrimaryButton buttonStyle={styles.optionButton} onPress={()=>setIsModalOpen(true)}>
-                            <Entypo name="dots-three-horizontal" size={24} color="black" />
+                        <PrimaryButton buttonStyle={styles.optionButton} onPress={() => setIsModalOpen(true)}>
+                            <Entypo name="dots-three-horizontal" size={24} color="black"/>
                         </PrimaryButton>
                         <PrimaryButton buttonStyle={styles.checkoutButton}
                                        pressableStyle={styles.checkoutButtonPressable}
---
+++ components/checkoutScreen/MiniActionTextModal.js
@@ -0,0 +1,247 @@
+import {
+  Modal,
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import Colors from "../../constants/Colors";
+import TextTheme from "../../constants/TextTheme";
+import Entypo from "@expo/vector-icons/Entypo";
+import CustomTextInput from "../../ui/CustomTextInput";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useState } from "react";
+import textTheme from "../../constants/TextTheme";
+import { SafeAreaView } from "react-native-safe-area-context";
+const MiniActionTextModal = (props) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+  };
+  return (
+    <Modal transparent={true} visible={props.isVisible} animationType="fade">
+      <TouchableOpacity
+        // onPress={props.onCloseModal}
+        activeOpacity={1}
+        style={styles.modalOverlay}
+      >
+        <KeyboardAvoidingView behavior="position" style={{ flex: 1, justifyContent:"flex-end" }}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalTitle}>
+              <Text></Text>
+              <Text style={[textTheme.titleLarge, styles.deleteClientText]}>
+                {props.title}
+              </Text>
+              <Entypo name="cross" size={24} color="black" onPress={props.onCloseModal} />
+            </View>
+            <FlatList
+              data={props.data}
+              style={styles.flatList}
+              renderItem={({ item }) => {
+                console.log(item);
+                return (
+                  <View style={{ marginTop: 10 }}>
+                    <Text style={TextTheme.bodyMedium}>{item.header}</Text>
+                    <View
+                      style={[
+                        styles.inputContainer,
+                        item.typeToggle !== 0 && {
+                          flexDirection: "row",
+                          alignItems: "center",
+                        },
+                      ]}
+                    >
+                      {item.boxType === "multiLineBox" ? (
+                        <View>
+                          <CustomTextInput type={"multiLine"} />
+                        </View>
+                      ) : item.boxType === "textBox" ? (
+                        <TextInput style={styles.textInputBox} />
+                      ) : item.boxType === "priceBox" ? (
+                        <View style={styles.priceBoxContainer}>
+                          <FontAwesome
+                            name="rupee"
+                            size={20}
+                            color="black"
+                            style={styles.rupeeIcon}
+                          />
+                          <TextInput style={styles.priceTextInput} />
+                        </View>
+                      ) : null}
+                      {item.typeToggle !== 0 ? (
+                        <View style={styles.toggleBox}>
+                          <TouchableOpacity
+                            style={[
+                              styles.optionButton,
+                              selectedOption === "percentage" &&
+                                styles.selectedOption,
+                            ]}
+                            onPress={() => handleOptionSelect("percentage")}
+                          >
+                            <FontAwesome6
+                              name="percentage"
+                              size={20}
+                              color={
+                                selectedOption === "percentage"
+                                  ? "white"
+                                  : "black"
+                              }
+                            />
+                          </TouchableOpacity>
+                          <TouchableOpacity
+                            style={[
+                              styles.optionButton,
+                              selectedOption === "rupee" &&
+                                styles.selectedOption,
+                            ]}
+                            onPress={() => handleOptionSelect("rupee")}
+                          >
+                            <FontAwesome
+                              name="rupee"
+                              size={20}
+                              color={
+                                selectedOption === "rupee" ? "white" : "black"
+                              }
+                            />
+                          </TouchableOpacity>
+                        </View>
+                      ) : null}
+                    </View>
+                  </View>
+                );
+              }}
+            />
+            <View style={styles.actionButton}>
+              <TouchableOpacity onPress={() => {}} style={styles.closeAction}>
+                <Text
+                  style={[
+                    { color: Colors.error, textAlign: "center" },
+                    TextTheme.labelLarge,
+                  ]}
+                >
+                  Close
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => {}} style={styles.saveAction}>
+                <Text
+                  style={[
+                    { color: Colors.white, textAlign: "center" },
+                    TextTheme.labelLarge,
+                  ]}
+                >
+                  Save
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </TouchableOpacity>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalOverlay: {
+    flex: 1,
+    height: "100%",
+    justifyContent: "flex-end",
+    backgroundColor: Colors.ripple,
+  },
+  modalContent: {
+    width: "100%",
+    backgroundColor: Colors.white,
+    // padding: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  modalTitle: {
+    elevation: 0.4,
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  flatList: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderColor: Colors.ripple,
+  },
+  inputContainer: {
+    marginTop: 10,
+  },
+  textInputBox: {
+    borderRadius: 5,
+    flex: 3,
+    height: 40,
+    borderWidth: 1,
+    borderColor: Colors.ripple,
+    marginRight: 10,
+  },
+  toggleBox: {
+    flexDirection: "row",
+    height: 40,
+    borderWidth: 1,
+    borderColor: Colors.ripple,
+    justifyContent: "space-between",
+    flex: 1,
+  },
+  optionButton: {
+    padding: 5,
+    flexGrow: 1,
+    alignItems: "center",
+    // padding: 10,
+    // borderRadius: 5,
+  },
+  selectedOption: {
+    flexGrow: 1,
+    backgroundColor: Colors.highlight,
+  },
+  actionButton: {
+    flexDirection: "row",
+    gap: 10,
+    justifyContent: "space-between",
+    margin: 20,
+    // borderTopWidth: 0.6,
+  },
+  closeAction: {
+    padding: 10,
+    borderWidth: 1,
+    flex: 3,
+    borderColor: Colors.ripple,
+    borderRadius: 6,
+  },
+  saveAction: {
+    padding: 10,
+    backgroundColor: Colors.green,
+    flex: 7,
+    borderRadius: 6,
+  },
+  priceBoxContainer: {
+    width: "97%",
+    height: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: Colors.ripple,
+    padding: 5,
+    borderRadius: 5,
+  },
+  rupeeIcon: {
+    fontWeight: 1,
+    marginRight: 10,
+    paddingHorizontal: 6,
+    borderRightColor:Colors.ripple,
+    borderRightWidth: 0.9,
+},
+  priceTextInput: {
+    flex: 1,
+    paddingVertical: 0,
+  },
+});
+
+export default MiniActionTextModal;
