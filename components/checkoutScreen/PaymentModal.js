025358ad0a89a354b172c41858e1f803830c5ecc
NaveenAkash-K <naveen.akash0904@gmail.com>
2024-10-08 18:03:09 +0530

    Bug fixes

--- components/checkoutScreen/PaymentModal.js
+++ components/checkoutScreen/PaymentModal.js
@@ -46,7 +46,8 @@ const PaymentModal = (props) => {
 
 
     const clientInfo = useSelector(state => state.clientInfo.details);
-    const isPrepaidAvailable = clientInfo.wallet_status && clientInfo.wallet_balance !== undefined && clientInfo.wallet_balance !== 0;
+    const isPrepaidInCart = useSelector(state => state.cart.prepaid_wallet[0].wallet_amount) !== "";
+    const isPrepaidAvailable = !isPrepaidInCart && clientInfo.wallet_status && clientInfo.wallet_balance !== undefined && clientInfo.wallet_balance !== 0;
     const [selectedPaymentOption, setSelectedPaymentOption] = useState(isPrepaidAvailable ? clientInfo.wallet_balance > props.price ? "prepaid" : "split_payment" : null);
     const [isInvoiceModalVisible, setIsInvoiceModalVisible] = useState(false);
     const [totalPrice, setTotalPrice] = useState(props.price);
